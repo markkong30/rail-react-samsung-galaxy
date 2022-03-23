a3db3cbdafcdb2d1695aa4e1cc4b8c36a94d3eb2
markkong30 <markkong30@gmail.com>
2022-03-24 00:41:54 +0800

    bug fixed

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,9 @@
-Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+# Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 
 
-# Rails.configuration.stripe = {
-#     :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-#     :secret_key      => ENV['STRIPE_SECRET_KEY']
-#   }
+Rails.configuration.stripe = {
+    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    :secret_key      => ENV['STRIPE_SECRET_KEY']
+  }
   
-#   Stripe.api_key = Rails.configuration.stripe[:secret_key]
+  Stripe.api_key = Rails.configuration.stripe[:secret_key]
